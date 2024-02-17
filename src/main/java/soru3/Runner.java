36175ce9f44d2f6b50a81f7f2bd294e692e92651
yasar.bal <ysr.bal@gmail.com>
2024-02-17 13:26:14 +0300

    first commit

--- src/main/java/soru3/Runner.java
+++ src/main/java/soru3/Runner.java
@@ -2,14 +2,18 @@ package soru3;
 
 import soru3.entity.Animal;
 import soru3.entity.Felis;
+import soru3.entity.FlyingAnimals;
 import soru3.entity.TerrestrialAnimals;
 
 public class Runner {
     public static void main(String[] args) {
         //Polimorphism ile ata sýnýftan bir child sýnýf nesnesi oluþturduk. Oluþturulan nesne ata sýnýfýn özelliklerini inheritance ile almaktadýr.
-        Animal lion = new Felis("Aslan","Wild");
+        Animal lion = new Felis("Aslan", "Wild");
+        Animal eagle = new FlyingAnimals("Kartal", "Wild");
         lion.move();
         lion.makeNoise();
+        eagle.move();
+        eagle.makeNoise();
 
     }
 }
