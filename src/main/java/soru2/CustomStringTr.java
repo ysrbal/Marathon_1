8f2b49b63d1b08d755e01af9585ec4a584973848
yasar.bal <ysr.bal@gmail.com>
2024-02-17 14:47:21 +0300

    first commit

--- src/main/java/soru2/CustomStringTr.java
+++ src/main/java/soru2/CustomStringTr.java
@@ -7,42 +7,90 @@ public class CustomStringTr {
         this.ifade = expression;
     }
 
+    /**
+     * Metot, String ifadenin uzunluðunu bulmayý saðlar.
+     * @return int
+     */
     public int uzunluk() {
         return ifade.length();
     }
 
+    /**
+     * Metot, String ifade içerisinde istenilen indexteki karakteri bulmauý saðlar.
+     * @param index --> Ýndex sayýsýný ifade eder.
+     * @return char
+     */
     public char karakterBul(int index) {
         return ifade.charAt(index);
     }
 
+    /**
+     * Metot, String ifadenin bütün karakterlerinin küçük harf olmasý saðlanýr.
+     * @return String
+     */
     public String hepsiKucukHarf() {
         return ifade.toLowerCase();
     }
 
+    /**
+     * Metot, String ifadenin bütün karakterlerinin büyük harf olmasý saðlanýr.
+     * @return String
+     */
     public String hepsiBuyukHarf() {
         return ifade.toUpperCase();
     }
 
+    /**
+     * Metot, ifade içerisinde istenilen içeriðin olup olmadýðýný bulmayý saðlar.
+     * @param icerik --> Ýfadenin içerisinde aranacak ifade
+     * @return boolean
+     */
     public boolean iceriyorMu(String icerik) {
         return ifade.contains(icerik);
     }
 
+    /**
+     * Metot, ifade içerisinde baþlangýç ve bitiþ indeksi arasýndaki içeriði döner.
+     *
+     * @param index  -> baþlangýç indeksini ifade eder.
+     * @param index2 -> bitiþ indeksini ifade eder.
+     * @return String
+     */
     public String altMetin(int index, int index2) {
         return ifade.substring(index, index2);
     }
 
+    /**
+     * Metot, bir deðeri baþka bir deðerle deðiþtirmeyi saðlar.
+     * @param s1 --> Deðiþecek deðeri ifade eder.
+     * @param s2 --> Yeni gelecek deðeri ifade eder.
+     * @return String
+     */
     public String degistir(String s1, String s2) {
         return ifade.replace(s1, s2);
     }
 
+    /**
+     * Metot, verilen deðerin kaçýncý indekste olduðunu bulmayý saðlar.
+     * @param s1 --> Indexi bulunacak deðeri ifade eder.
+     * @return int
+     */
     public int kacinciIndex(String s1) {
         return ifade.indexOf(s1);
     }
 
+    /**
+     * Metot, ifade içeriðinin boþ olup olmadýðýný bulmayý saðlar.
+     * @return boolean
+     */
     public boolean bosMu() {
         return ifade.isEmpty();
     }
 
+    /**
+     * Metot, ifadenin içerisinde boþluk olup olmadýðýný bulmayý saðlar.
+     * @return boolean
+     */
     public boolean boslukVarMi() {
         return ifade.isBlank();
     }
--- src/main/java/soru2/Runner.java
+++ src/main/java/soru2/Runner.java
@@ -7,13 +7,12 @@ public class Runner {
         System.out.println("Ýfadenin 6. karakteri ................ : " + ifade.karakterBul(6));
         System.out.println("Ýfadenin hepsi küçük harf ............ : " + ifade.hepsiKucukHarf());
         System.out.println("Ýfadenin hepsi büyük harf ............ : " + ifade.hepsiBuyukHarf());
-        System.out.println("Ýfade içerisinde içeriyor mu? ........ :" + ifade.iceriyorMu("tirici"));
-        System.out.println("Ýfadenin 9 ve 16. indexleri arasý .... :" + ifade.altMetin(9, 16));
+        System.out.println("Ýfade içerisinde içeriyor mu? ........ : " + ifade.iceriyorMu("tirici"));
+        System.out.println("Ýfadenin 9 ve 16. indexleri arasý .... : " + ifade.altMetin(9, 16));
         System.out.println("Ýfadede f yerine a gelecek ........... : " + ifade.degistir("f", "a"));
-        System.out.println("Ýfade içerisinde ilk kaçýncý indexte ? :" + ifade.kacinciIndex("c"));
-        System.out.println("Ýfade tamamen boþ mu ? ............... :" + ifade.bosMu());
+        System.out.println("Ýfade içerisinde ilk kaçýncý indexte ? : " + ifade.kacinciIndex("c"));
+        System.out.println("Ýfade tamamen boþ mu ? ............... : " + ifade.bosMu());
         System.out.println("Ýfadede boþluk var mý? ............... : " + ifade.boslukVarMi());
-
 
     }
 }
